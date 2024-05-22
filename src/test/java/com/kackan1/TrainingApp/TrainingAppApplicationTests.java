db8101535b1438443396acf20a33fbdd32d8a8f5
Kackan1 <Kacper.Kaniuka@wp.pl>
2024-05-22 17:32:50 +0200

    first commit

---
+++ src/test/java/com/kackan1/TrainingApp/TrainingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kackan1.TrainingApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
